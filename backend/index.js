7cf8e8559f6dfbf9b1032a9077696d1ae212d67c
root <root@ip-172-31-37-58.ap-south-1.compute.internal>
2026-06-17 09:59:31 +0000

    Update backend index.js

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,13 @@ app.get("/", (req, res) => {
   res.send("Hello from Backend API 🚀");
 });
 
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "UP",
+    service: "backend"
+  });
+});
+
 app.listen(5000, () => {
   console.log("Backend running on port 5000");
 });
